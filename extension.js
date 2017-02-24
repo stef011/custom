317f2007c6cab241122fc15bea2fabf7bd1db340
stef011 <zundel.stephane@sfr.fr>
2017-02-24 15:43:33 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -112,6 +112,6 @@
     }));
 
     // Start the bot and extend it when it has loaded.
-    $.getScript("https://rawgit.com/basicBot/source/master/basicBot.js", extend);
+    $.getScript("https://rawgit.com/stef011/source/master/basicBot.js", extend);
 
 }).call(this);
